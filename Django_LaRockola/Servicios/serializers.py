ded553ca41917277af1888b9b97461093cf5d9c3
FEDERICK TURIZO <federick.turizo@gmail.com>
2021-09-16 20:04:45 -0500

    Serializers Version 0.1

--- Django_LaRockola/Servicios/serializers.py
+++ Django_LaRockola/Servicios/serializers.py
@@ -2,17 +2,25 @@ from django.db.models import fields
 from django.db.models.base import Model
 from rest_framework import serializers
 from django.contrib.auth.models import User, Group
+from Servicios.models import *
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta():
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
+        model = Usuario
+        fields = "__all__"
+
+class GenderSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = GeneroMusical
+        fields = "__all__"
+
+class SongsSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Canciones
+        fields = "__all__"
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
+
 
 
 
